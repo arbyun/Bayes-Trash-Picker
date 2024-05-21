1e71b46bfd881f3f33c233874dded7cce2494daa
David Mendes <david.f.mendes@proton.me>
2024-05-21 17:39:51 +0100

    Updated Cell prefab and removed unused code in Cell class

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,16 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
     public enum State {Empty, HasTrash, Wall}
-    [SerializeField] private GameObject playerImage;
     [SerializeField] private GameObject trashImage;
 
     private State state;
-    private bool hasPlayer;
     private Vector2 coordinates;
     public State CellState { get => state; set{ state = value; } }
     public Vector2 Coordinates { get => coordinates; set{ coordinates = value; } }
@@ -18,7 +14,6 @@ public class Cell : MonoBehaviour
     private void Start()
     {
         state = State.Empty;
-        hasPlayer = false;
         coordinates = new Vector2(0, 0);
     }
 
@@ -27,23 +22,14 @@ public class Cell : MonoBehaviour
         if(state == State.Wall)
         {
             gameObject.GetComponent<Image>().enabled = false;
-            playerImage.SetActive(false);
             trashImage.SetActive(false);
         }
 
         else
         {
             gameObject.GetComponent<Image>().enabled = true;
-
-            playerImage.SetActive(hasPlayer);
+            
             trashImage.SetActive(state == State.HasTrash);
         }
-    }
-
-    public void PlayerState(bool inTheTile)
-    {
-        hasPlayer = inTheTile;
-
-        UpdateCell();
     }
 }
