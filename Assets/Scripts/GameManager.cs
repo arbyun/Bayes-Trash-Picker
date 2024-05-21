0f204e2a5aefa2713ce33a758299df482cf6f2b5
David Mendes <david.f.mendes@proton.me>
2024-05-21 17:40:33 +0100

    Fixed typos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private UIManager uiManager;
+    [SerializeField] private GameObject lusoPrefab;
     [SerializeField] private GameObject cellPrefab;
     [SerializeField] private Transform cellGrid;
     [SerializeField] private Transform cellBG;
     [SerializeField] private int numOfRows = 5;
-    [SerializeField] private int numOfCollumns = 5;
+    [SerializeField] private int numOfColumns = 5;
     [SerializeField] private int numOfSteps = 20;
-    [SerializeField] private GameObject LusoPrefab;
     
     [Range(0, 100)]
     [SerializeField] private int probabilityOfTrash = 40;
@@ -30,7 +28,7 @@ public class GameManager : MonoBehaviour
     {
         score = 0;
         stepsLeft = numOfSteps;
-        cellBG.GetComponent<RectTransform>().sizeDelta = new Vector2(numOfCollumns * 75, numOfRows * 75);
+        cellBG.GetComponent<RectTransform>().sizeDelta = new Vector2(numOfColumns * 75, numOfRows * 75);
 
         cellList = new List<Cell>(numOfRows * numOfSteps);
 
@@ -43,7 +41,7 @@ public class GameManager : MonoBehaviour
     {
         for(int i = 0; i < numOfRows + 2; i++)
         {
-            for(int j = 0; j < numOfCollumns + 2; j++)
+            for(int j = 0; j < numOfColumns + 2; j++)
             {
                 Cell newCell = Instantiate(cellPrefab, cellGrid).
                     GetComponent<Cell>();
@@ -51,8 +49,8 @@ public class GameManager : MonoBehaviour
                 newCell.Coordinates = new Vector2(j - 1, i - 1);
                 newCell.name = $"({j - 1},{i - 1})";
 
-                if(i == 0 || i == numOfCollumns + 1 || j == 0 || 
-                    j == numOfCollumns + 1)
+                if(i == 0 || i == numOfColumns + 1 || j == 0 || 
+                    j == numOfColumns + 1)
                 {
                     newCell.CellState = Cell.State.Wall;
                 }
@@ -85,10 +83,10 @@ public class GameManager : MonoBehaviour
                 availableCells.Add(currentCell);
             }
         }
-
         int startingCell = Random.Range(0, availableCells.Count);
-        LusoBehaviour lusoBehaviour = Instantiate(LusoPrefab).GetComponent<LusoBehaviour>();
+        
+        LusoBehaviour lusoBehaviour = Instantiate(lusoPrefab).GetComponent<LusoBehaviour>();
         lusoBehaviour.UpdateCurrentCell(startingCell);
-        lusoBehaviour.UpdateGridDimensions(new Vector2(numOfRows, numOfCollumns));
+        lusoBehaviour.UpdateGridDimensions(new Vector2(numOfRows, numOfColumns));
     }
 }
