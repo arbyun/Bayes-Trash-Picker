6c991c459df1bcdd0368061d04c85baa24f82aa9
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-24 11:41:53 +0100

    add behaviour-defining AI class

---
+++ Assets/Scripts/LusoAI/LusoAIBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using LusoAI;
+using UnityEngine;
+
+public class LusoAIBehaviour : MonoBehaviour
+{
+    private GameManager _gm;
+    private NaiveBayesClassifier _classifier;
+    private LusoBehaviour _luso;
+
+    private Coroutine _play;
+
+    private void Start()
+    {
+        _gm = FindObjectOfType<GameManager>();
+        _luso = FindObjectOfType<LusoBehaviour>();
+        _classifier = _gm.NaiveBayesClassifier;
+    }
+
+    private IEnumerator AIPlay()
+    {
+        var neighboringCells = _luso.GetNeighboringCells();
+        Action action = _classifier.Predict(neighboringCells);
+        action?.Invoke();
+        _gm.ActionPlayed();
+
+        yield return new WaitForSeconds(0.5f);
+        _play = null;
+    }
+
+    public void Play()
+    {
+        _play ??= StartCoroutine(AIPlay());
+    }
+}
