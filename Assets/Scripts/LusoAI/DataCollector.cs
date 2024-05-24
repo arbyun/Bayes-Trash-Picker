7dd9dac743376fc1c8cf774b52efd78a0d248aeb
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-24 10:08:58 +0100

    create data collector class for recording data

---
+++ Assets/Scripts/LusoAI/DataCollector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollector : MonoBehaviour
+{
+    private readonly List<TrainingData> _trainingDataList = new();
+
+    public void RecordAction(int currentCellIndex, Cell.State[] neighboringCells, string action)
+    {
+        _trainingDataList.Add(new TrainingData(currentCellIndex, neighboringCells, action));
+    }
+
+    public List<TrainingData> GetTrainingData()
+    {
+        return _trainingDataList;
+    }
+}
