4ecf52928ebb75f11e15b32d243bd797c28ef928
arbyun <115217686+arbyun@users.noreply.github.com>
2024-05-24 10:08:35 +0100

    create training data struct for easy collection of data

---
+++ Assets/Scripts/LusoAI/TrainingData.cs
@@ -0,0 +1,13 @@
+﻿public struct TrainingData
+{
+    public int CellIndex { get; set; }
+    public Cell.State[] NeighboringCells { get; set; }
+    public string Action { get; set; }
+
+    public TrainingData(int cellIndex, Cell.State[] neighboringCells, string action)
+    {
+        CellIndex = cellIndex;
+        NeighboringCells = neighboringCells;
+        Action = action;
+    }
+}
